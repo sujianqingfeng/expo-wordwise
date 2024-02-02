d42046e7722c5a3879135c91d52c5f3afa0e883d
hens <121116111@qq.com>
2024-02-03 00:04:07 +0800

    feat: add nativewind

---
+++ components/Test.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native"
+
+
+export default function Test(){
+  return (<View>
+    <Text className="text-red-300">
+        test text
+      </Text>
+  </View>)
+}
+
